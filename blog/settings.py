541a95af7745ac7b4890d8ddb33008b577c9be75
ishakhanal <ishakhanal25@gmail.com>
2019-12-10 18:46:13 +0545

    herokudeploytry

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('BLOG_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1', ]
 
 
 # Application definition
